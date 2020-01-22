e4011e437a2f0883b29a259a1ded9ca35f39a11b
na_th_an <na_th_an@hotmail.com>
2020-01-22 17:12:14 +0100

    Helmet multilevel wip 1

---
+++ examples/helmet/dev/build_assets.bat
@@ -0,0 +1,6 @@
+@echo off
+
+..\utils\ts2bin.exe nofont ..\gfx\work0.png tileset0.bin 7 >nul
+..\utils\ts2bin.exe nofont ..\gfx\work1.png tileset1.bin 7 >nul
+..\utils\ts2bin.exe nofont ..\gfx\work2.png tileset2.bin 7 >nul
+
