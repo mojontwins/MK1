704a94e1beb61972ac95f643f7949f0975c99fe7
na_th_an <na_th_an@hotmail.com>
2023-11-18 08:35:28 +0100

    v5.10

--- examples/impossamal-128/dev/compile.bat
+++ examples/impossamal-128/dev/compile.bat
@@ -10,7 +10,7 @@ if [%1]==[clean] goto :clean
 cd ..\script
 if not exist %game%.spt goto :noscript
 echo Compilando script
-..\utils\msc3_mk1.exe %game%.spt 25 > nul
+..\..\..\src\utils\msc3_mk1.exe %game%.spt 25 > nul
 copy msc.h ..\dev\my > nul
 copy msc-config.h ..\dev\my > nul
 copy scripts.bin ..\dev\ > nul
@@ -20,48 +20,48 @@ cd ..\dev
 if [%1]==[justscripts] goto :compile
 
 echo Convirtiendo mapa
-..\utils\mapcnv.exe ..\map\mapa.map assets\mapa.h 25 1 15 10 15 > nul
+..\..\..\src\utils\mapcnv.exe ..\map\mapa.map assets\mapa.h 25 1 15 10 15 > nul
 
 echo Convirtiendo enemigos/hotspots
-..\utils\ene2h.exe ..\enems\enems.ene assets\enems.h
+..\..\..\src\utils\ene2h.exe ..\enems\enems.ene assets\enems.h
 
 echo Importando GFX
-..\utils\ts2bin.exe ..\gfx\font.png ..\gfx\work.png tileset.bin 4 >nul
+..\..\..\src\utils\ts2bin.exe ..\gfx\font.png ..\gfx\work.png tileset.bin 4 >nul
 
-..\utils\sprcnv.exe ..\gfx\sprites.png assets\sprites.h > nul
+..\..\..\src\utils\sprcnv.exe ..\gfx\sprites.png assets\sprites.h > nul
 
-..\utils\sprcnvbin.exe ..\gfx\sprites_extra.png sprites_extra.bin 1 > nul
-..\utils\sprcnvbin8.exe ..\gfx\sprites_bullet.png sprites_bullet.bin 1 > nul
+..\..\..\src\utils\sprcnvbin.exe ..\gfx\sprites_extra.png sprites_extra.bin 1 > nul
+..\..\..\src\utils\sprcnvbin8.exe ..\gfx\sprites_bullet.png sprites_bullet.bin 1 > nul
 
-..\utils\png2scr.exe ..\gfx\title.png ..\gfx\title.scr > nul
-..\utils\png2scr.exe ..\gfx\marco.png ..\gfx\marco.scr > nul
-..\utils\png2scr.exe ..\gfx\ending.png ..\gfx\ending.scr > nul
-..\utils\png2scr.exe ..\gfx\loading.png loading.bin > nul
-..\utils\apultra.exe ..\gfx\title.scr ..\bin\title.bin > nul
-..\utils\apultra.exe ..\gfx\marco.scr ..\bin\marco.bin > nul
-..\utils\apultra.exe ..\gfx\ending.scr ..\bin\ending.bin > nul
+..\..\..\src\utils\png2scr.exe ..\gfx\title.png ..\gfx\title.scr > nul
+..\..\..\src\utils\png2scr.exe ..\gfx\marco.png ..\gfx\marco.scr > nul
+..\..\..\src\utils\png2scr.exe ..\gfx\ending.png ..\gfx\ending.scr > nul
+..\..\..\src\utils\png2scr.exe ..\gfx\loading.png loading.bin > nul
+..\..\..\src\utils\apultra.exe ..\gfx\title.scr ..\bin\title.bin > nul
+..\..\..\src\utils\apultra.exe ..\gfx\marco.scr ..\bin\marco.bin > nul
+..\..\..\src\utils\apultra.exe ..\gfx\ending.scr ..\bin\ending.bin > nul
 
 if [%1]==[justassets] goto :end
 
 echo Compilando textos
-..\utils\textstuffer.exe ..\texts\texts.txt texts.bin 24
+..\..\..\src\utils\textstuffer.exe ..\texts\texts.txt texts.bin 24
 
 echo Running The Librarian
-..\utils\librarian2.exe list=..\bin\list.txt index=assets\librarian.h bins_prefix=..\bin\ rams_prefix=..\bin\ > nul
+..\..\..\src\utils\librarian2.exe list=..\bin\list.txt index=assets\librarian.h bins_prefix=..\bin\ rams_prefix=..\bin\ > nul
 
 rem *** Música AY: Descomenta el player que vayas a usar (Wyz o Arkos) ***
 
 rem echo Compilando musica 128k - Wyz Player
 rem cd ..\mus
-rem ..\utils\apultra.exe menu.mus menu.bin
-rem ..\utils\apultra.exe level1.mus level1.bin
-rem ..\utils\pasmo WYZproPlay47aZXc.ASM ..\bin\RAM1.bin 
+rem ..\..\..\src\utils\apultra.exe menu.mus menu.bin
+rem ..\..\..\src\utils\apultra.exe level1.mus level1.bin
+rem ..\..\..\src\utils\pasmo WYZproPlay47aZXc.ASM ..\bin\RAM1.bin 
 rem cd ..\dev
 
 echo Compilando musica 128k - Arkos Player
 cd ..\mus_arkos
 if [%1]==[nomus] goto :nomus
-..\utils\build_mus_bin.exe ram1.bin > nul
+..\..\..\src\utils\build_mus_bin.exe ram1.bin > nul
 :nomus
 copy ram1.bin ..\bin
 copy arkos-addresses.h ..\dev\sound
@@ -71,29 +71,29 @@ cd ..\dev
 echo Compilando guego
 zcc +zx -vn mk1.c -O3 -crt0=crt.asm -o %game%.bin -lsplib2_mk2.lib -zorg=24000 > nul
 rem zcc +zx -vn mk1.c -o %game%.bin -lsplib2_mk2.lib -zorg=24000 > nul
-..\utils\printsize.exe %game%.bin
-..\utils\printsize.exe scripts.bin
+..\..\..\src\utils\printsize.exe %game%.bin
+..\..\..\src\utils\printsize.exe scripts.bin
 
 rem *** Tipo de cargador ***
 
 rem echo Construyendo cinta
 rem cambia LOADER por el nombre que quieres que salga en Program:
-rem ..\utils\bas2tap -a10 -sIMPOSSAMAL loader\loader.bas loader.tap > nul
-rem ..\utils\bin2tap -o screen.tap -a 16384 loading.bin > nul
-rem ..\utils\bin2tap -o main.tap -a 24000 %game%.bin > nul
+rem ..\..\..\src\utils\bas2tap -a10 -sIMPOSSAMAL loader\loader.bas loader.tap > nul
+rem ..\..\..\src\utils\bin2tap -o screen.tap -a 16384 loading.bin > nul
+rem ..\..\..\src\utils\bin2tap -o main.tap -a 24000 %game%.bin > nul
 rem copy /b loader.tap + screen.tap + main.tap %game%.tap > nul
 	echo Construyendo cinta 128k
-	..\utils\imanol.exe ^
+	..\..\..\src\utils\imanol.exe ^
 		in=loader\loaderzx.asm-orig ^
 		out=loader\loader.asm ^
 		ram1_length=?..\bin\RAM1.bin ^
 		ram3_length=?..\bin\RAM3.bin ^
 		mb_length=?%game%.bin  > nul
 
-	..\utils\pasmo.exe loader\loader.asm ..\bin\loader.bin loader.txt
+	..\..\..\src\utils\pasmo.exe loader\loader.asm ..\bin\loader.bin loader.txt
 
 	rem cambia LOADER por el nombre que quieres que salga en Program:
-	..\utils\GenTape.exe %game%.tap ^
+	..\..\..\src\utils\GenTape.exe %game%.tap ^
 		basic 'IMPOSSAMAL128' 10 ..\bin\loader.bin ^
 		data                loading.bin ^
 		data                ..\bin\RAM1.bin ^
--- examples/impossamal/dev/compile.bat
+++ examples/impossamal/dev/compile.bat
@@ -10,7 +10,7 @@ if [%1]==[clean] goto :clean
 cd ..\script
 if not exist %game%.spt goto :noscript
 echo Compilando script
-..\utils\msc3_mk1.exe %game%.spt 25 > nul
+..\..\..\src\utils\msc3_mk1.exe %game%.spt 25 > nul
 copy msc.h ..\dev\my > nul
 copy msc-config.h ..\dev\my > nul
 copy scripts.bin ..\dev\ > nul
@@ -20,48 +20,48 @@ cd ..\dev
 if [%1]==[justscripts] goto :compile
 
 echo Convirtiendo mapa
-..\utils\mapcnv.exe ..\map\mapa.map assets\mapa.h 25 1 15 10 15 > nul
+..\..\..\src\utils\mapcnv.exe ..\map\mapa.map assets\mapa.h 25 1 15 10 15 > nul
 
 echo Convirtiendo enemigos/hotspots
-..\utils\ene2h.exe ..\enems\enems.ene assets\enems.h
+..\..\..\src\utils\ene2h.exe ..\enems\enems.ene assets\enems.h
 
 echo Importando GFX
-..\utils\ts2bin.exe ..\gfx\font.png ..\gfx\work.png tileset.bin 4 >nul
+..\..\..\src\utils\ts2bin.exe ..\gfx\font.png ..\gfx\work.png tileset.bin 4 >nul
 
-..\utils\sprcnv.exe ..\gfx\sprites.png assets\sprites.h > nul
+..\..\..\src\utils\sprcnv.exe ..\gfx\sprites.png assets\sprites.h > nul
 
-..\utils\sprcnvbin.exe ..\gfx\sprites_extra.png sprites_extra.bin 1 > nul
-..\utils\sprcnvbin8.exe ..\gfx\sprites_bullet.png sprites_bullet.bin 1 > nul
+..\..\..\src\utils\sprcnvbin.exe ..\gfx\sprites_extra.png sprites_extra.bin 1 > nul
+..\..\..\src\utils\sprcnvbin8.exe ..\gfx\sprites_bullet.png sprites_bullet.bin 1 > nul
 
-..\utils\png2scr.exe ..\gfx\title.png ..\gfx\title.scr > nul
-..\utils\png2scr.exe ..\gfx\marco.png ..\gfx\marco.scr > nul
-..\utils\png2scr.exe ..\gfx\ending.png ..\gfx\ending.scr > nul
-..\utils\png2scr.exe ..\gfx\loading.png loading.bin > nul
-..\utils\apultra.exe ..\gfx\title.scr ..\bin\title.bin > nul
-..\utils\apultra.exe ..\gfx\marco.scr ..\bin\marco.bin > nul
-..\utils\apultra.exe ..\gfx\ending.scr ..\bin\ending.bin > nul
+..\..\..\src\utils\png2scr.exe ..\gfx\title.png ..\gfx\title.scr > nul
+..\..\..\src\utils\png2scr.exe ..\gfx\marco.png ..\gfx\marco.scr > nul
+..\..\..\src\utils\png2scr.exe ..\gfx\ending.png ..\gfx\ending.scr > nul
+..\..\..\src\utils\png2scr.exe ..\gfx\loading.png loading.bin > nul
+..\..\..\src\utils\apultra.exe ..\gfx\title.scr ..\bin\title.bin > nul
+..\..\..\src\utils\apultra.exe ..\gfx\marco.scr ..\bin\marco.bin > nul
+..\..\..\src\utils\apultra.exe ..\gfx\ending.scr ..\bin\ending.bin > nul
 
 if [%1]==[justassets] goto :end
 
 echo Compilando textos
-..\utils\textstuffer.exe ..\texts\texts.txt texts.bin 24
+..\..\..\src\utils\textstuffer.exe ..\texts\texts.txt texts.bin 24
 
 rem echo Running The Librarian
-rem ..\utils\librarian2.exe list=..\bin\list.txt index=assets\librarian.h bins_prefix=..\bin\ rams_prefix=..\bin\ > nul
+rem ..\..\..\src\utils\librarian2.exe list=..\bin\list.txt index=assets\librarian.h bins_prefix=..\bin\ rams_prefix=..\bin\ > nul
 
 rem *** Música AY: Descomenta el player que vayas a usar (Wyz o Arkos) ***
 
 rem echo Compilando musica 128k - Wyz Player
 rem cd ..\mus
-rem ..\utils\apultra.exe menu.mus menu.bin
-rem ..\utils\apultra.exe level1.mus level1.bin
-rem ..\utils\pasmo WYZproPlay47aZXc.ASM ..\bin\RAM1.bin 
+rem ..\..\..\src\utils\apultra.exe menu.mus menu.bin
+rem ..\..\..\src\utils\apultra.exe level1.mus level1.bin
+rem ..\..\..\src\utils\pasmo WYZproPlay47aZXc.ASM ..\bin\RAM1.bin 
 rem cd ..\dev
 
 rem echo Compilando musica 128k - Arkos Player
 rem cd ..\mus_arkos
 rem if [%1]==[nomus] goto :nomus
-rem ..\utils\build_mus_bin.exe ram1.bin > nul
+rem ..\..\..\src\utils\build_mus_bin.exe ram1.bin > nul
 rem :nomus
 rem copy ram1.bin ..\bin
 rem copy arkos-addresses.h ..\dev\sound
@@ -71,29 +71,29 @@ rem cd ..\dev
 echo Compilando guego
 zcc +zx -vn mk1.c -O3 -crt0=crt.asm -o %game%.bin -lsplib2_mk2.lib -zorg=24000 > nul
 rem zcc +zx -vn mk1.c -o %game%.bin -lsplib2_mk2.lib -zorg=24000 > nul
-..\utils\printsize.exe %game%.bin
-..\utils\printsize.exe scripts.bin
+..\..\..\src\utils\printsize.exe %game%.bin
+..\..\..\src\utils\printsize.exe scripts.bin
 
 rem *** Tipo de cargador ***
 
 echo Construyendo cinta
 rem cambia LOADER por el nombre que quieres que salga en Program:
-..\utils\bas2tap -a10 -sIMPOSSAMAL loader\loader.bas loader.tap > nul
-..\utils\bin2tap -o screen.tap -a 16384 loading.bin > nul
-..\utils\bin2tap -o main.tap -a 24000 %game%.bin > nul
+..\..\..\src\utils\bas2tap -a10 -sIMPOSSAMAL loader\loader.bas loader.tap > nul
+..\..\..\src\utils\bin2tap -o screen.tap -a 16384 loading.bin > nul
+..\..\..\src\utils\bin2tap -o main.tap -a 24000 %game%.bin > nul
 copy /b loader.tap + screen.tap + main.tap %game%.tap > nul
 rem echo Construyendo cinta 128k
-rem ..\utils\imanol.exe ^
+rem ..\..\..\src\utils\imanol.exe ^
 rem in=loader\loaderzx.asm-orig ^
 rem out=loader\loader.asm ^
 rem ram1_length=?..\bin\RAM1.bin ^
 rem ram3_length=?..\bin\RAM3.bin ^
 rem mb_length=?%game%.bin  > nul
 
-rem ..\utils\pasmo.exe loader\loader.asm ..\bin\loader.bin loader.txt
+rem ..\..\..\src\utils\pasmo.exe loader\loader.asm ..\bin\loader.bin loader.txt
 
 rem cambia LOADER por el nombre que quieres que salga en Program:
-rem ..\utils\GenTape.exe %game%.tap ^
+rem ..\..\..\src\utils\GenTape.exe %game%.tap ^
 rem basic 'LOADER' 10 ..\bin\loader.bin ^
 rem data                loading.bin ^
 rem data                ..\bin\RAM1.bin ^
